6729eed83cba2b06ae60f75fb9baa24b23ad02b7
bossacode <hlee2745@gmail.com>
2023-12-19 21:31:32 +0900

    generate train val test data seperately

--- generate_data.py
+++ generate_data.py
@@ -9,13 +9,16 @@ import os
 
 # First, we construct 10 base functions: $\cos(k \pi t)$ for $k = 1, \dots, 10$
 # For each data, we sum 3 randomly sampled base functions and add Gaussian noise from $N(0, \sigma^2)$
-def generate_my_data(tseq, sig=0.5, n=4000, scale=True):
+def generate_my_data(tseq, sig=0.5, n=5000, split_size=[0.6, 0.2, 0.2], scale=True):
     """
     tseq: timepoints
     sig: std of Gaussian distribution
     n: size of data
+    split_size: list containing proportion of training data, validation data, test data
     scale: whether to standardize the data
     """
+    assert sum(split_size) == 1
+
     base_func = [torch.cos(i * torch.pi * tseq) for i in range(10)]
     data_list = []
     for i in range(n):
@@ -31,71 +34,87 @@ def generate_my_data(tseq, sig=0.5, n=4000, scale=True):
         original_data = torch.from_numpy(scaler.fit_transform(original_data.numpy()))
         noise_data = torch.from_numpy(scaler.fit_transform(noise_data.numpy()))
 
+    # train/val/test split
+    num_split_data = [int(n*p) for p in split_size]
+    leftover = n - sum(num_split_data)
+    if leftover != 0:
+        for i in range(leftover):
+            num_split_data[i] += 1
+    original_train, original_val, original_test = original_data.split(num_split_data)
+    noise_train, noise_val, noise_test = noise_data.split(num_split_data)
+
+    # save data
     if not os.path.exists("data/mydata/"):
-        os.makedirs("data/mydata/")
-    torch.save(original_data, "data/mydata/original_data.pt")
-    torch.save(noise_data, "data/mydata/noise_data.pt")
+        os.makedirs("data/mydata/train/")
+        os.makedirs("data/mydata/val/")
+        os.makedirs("data/mydata/test/")
+    torch.save(original_train, "data/mydata/train/original_train.pt")
+    torch.save(noise_train, "data/mydata/train/noise_train.pt")
+    torch.save(original_val, "data/mydata/val/original_val.pt")
+    torch.save(noise_val, "data/mydata/val/noise_val.pt")
+    torch.save(original_test, "data/mydata/test/original_test.pt")
+    torch.save(noise_test, "data/mydata/test/noise_test.pt")
 
 
-def generate_matern_data(tseq, nu=0.5, sig=1, n=4000, scale=True):
-    """
-    tseq: timepoints
-    nu:
-    sig: std of Gaussian distribution
-    n: size of data
-    scale: whether to standardize the data
-    """
-    tseq = tseq.numpy()
-    h = tseq[1:] - tseq[:-1]    # length of intervals
+# def generate_matern_data(tseq, nu=0.5, sig=1, n=4000, scale=True):
+#     """
+#     tseq: timepoints
+#     nu:
+#     sig: std of Gaussian distribution
+#     n: size of data
+#     scale: whether to standardize the data
+#     """
+#     tseq = tseq.numpy()
+#     h = tseq[1:] - tseq[:-1]    # length of intervals
 
-    # Matern Process with nu = 1/2
-    kernel = sig**2 * Matern(nu=nu)
-    cov_mat = kernel(tseq.reshape(T,1), tseq.reshape(T,1))
+#     # Matern Process with nu = 1/2
+#     kernel = sig**2 * Matern(nu=nu)
+#     cov_mat = kernel(tseq.reshape(T,1), tseq.reshape(T,1))
 
-    z = np.random.randn(n, T)
-    data = z @ sqrtm(cov_mat)   # [data_size, T]
+#     z = np.random.randn(n, T)
+#     data = z @ sqrtm(cov_mat)   # [data_size, T]
     
-    # standardize data
-    if scale:
-        scaler = StandardScaler()
-        data = torch.from_numpy(scaler.fit_transform(data)).float()
+#     # standardize data
+#     if scale:
+#         scaler = StandardScaler()
+#         data = torch.from_numpy(scaler.fit_transform(data)).float()
 
-    if not os.path.exists("data/matern/"):
-        os.makedirs("data/matern/")
-    torch.save(data, "data/matern/data.pt")
+#     if not os.path.exists("data/matern/"):
+#         os.makedirs("data/matern/")
+#     torch.save(data, "data/matern/data.pt")
 
 
-def generate_adafnn_data(tseq, sig=1, n=4000, scale=True):
-    """
-    tseq: timepoints
-    sig: std of Gaussian distribution
-    n: size of data
-    scale: whether to standardize the data
-    """
-    K = 5
-    # case 3 of AdaFNN paper
-    z = torch.ones(n, K)
-    z[:, 0] = z[:, 2] = 5
-    # z[:, 4] = z[:, 9] = 3
-    z[:, 4] = 3
-    r = (sqrt(3) + sqrt(3)) * torch.rand(n, K) - sqrt(3)   # uniform from [-sqrt(3), sqrt(3)]
-    c = z*r
-    phi = torch.stack([torch.ones(len(tseq)) if k == 1
-                       else sqrt(2) * torch.cos((k-1) * torch.pi * tseq)
-                       for k in range(1, K+1)])
-    original_data = torch.matmul(c, phi)
-    noise_data = original_data + sig * torch.randn(n, len(tseq))    # add gaussian noise from N(0, sig^2)
+# def generate_adafnn_data(tseq, sig=1, n=4000, scale=True):
+#     """
+#     tseq: timepoints
+#     sig: std of Gaussian distribution
+#     n: size of data
+#     scale: whether to standardize the data
+#     """
+#     K = 5
+#     # case 3 of AdaFNN paper
+#     z = torch.ones(n, K)
+#     z[:, 0] = z[:, 2] = 5
+#     # z[:, 4] = z[:, 9] = 3
+#     z[:, 4] = 3
+#     r = (sqrt(3) + sqrt(3)) * torch.rand(n, K) - sqrt(3)   # uniform from [-sqrt(3), sqrt(3)]
+#     c = z*r
+#     phi = torch.stack([torch.ones(len(tseq)) if k == 1
+#                        else sqrt(2) * torch.cos((k-1) * torch.pi * tseq)
+#                        for k in range(1, K+1)])
+#     original_data = torch.matmul(c, phi)
+#     noise_data = original_data + sig * torch.randn(n, len(tseq))    # add gaussian noise from N(0, sig^2)
 
-    # standardize data
-    if scale:
-        scaler = StandardScaler()
-        original_data = torch.from_numpy(scaler.fit_transform(original_data.numpy()))
-        noise_data = torch.from_numpy(scaler.fit_transform(noise_data.numpy()))
+#     # standardize data
+#     if scale:
+#         scaler = StandardScaler()
+#         original_data = torch.from_numpy(scaler.fit_transform(original_data.numpy()))
+#         noise_data = torch.from_numpy(scaler.fit_transform(noise_data.numpy()))
 
-    if not os.path.exists("data/adafnn/"):
-        os.makedirs("data/adafnn/")
-    torch.save(original_data, "data/adafnn/original_data.pt")
-    torch.save(noise_data, "data/adafnn/noise_data.pt")
+#     if not os.path.exists("data/adafnn/"):
+#         os.makedirs("data/adafnn/")
+#     torch.save(original_data, "data/adafnn/original_data.pt")
+#     torch.save(noise_data, "data/adafnn/noise_data.pt")
 
 
 if __name__ == "__main__":
@@ -104,11 +123,11 @@ if __name__ == "__main__":
 
     T = 101 # number of timepoints
     tseq = torch.linspace(0, 1, T)  # timepoints
-    n = 10000   # number of data
+    n = 5000   # number of data
 
     if not os.path.exists("data/"):
         os.makedirs("data/")
 
     generate_my_data(tseq, n=n)
-    generate_matern_data(tseq, n=n)
-    generate_adafnn_data(tseq, n=n)
+    # generate_matern_data(tseq, n=n)
+    # generate_adafnn_data(tseq, n=n)
--- train.py
+++ train.py
@@ -3,31 +3,19 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import Adam
 from torch.optim.lr_scheduler import StepLR
-from sklearn.model_selection import train_test_split
 import wandb
-from adafnn import _l2
 
 
 class CustomDataset(Dataset):
-    def __init__(self, x_data_path="data/original_data.pt", y_data_path="data/original_data.pt", mode="train", val_size=0.3):
-        assert mode in ("train", "val")
-        self.mode = mode
-        x_data = torch.load(x_data_path)
-        y_data = torch.load(y_data_path)
-        self.x_tr, self.x_val, self.y_tr, self.y_val = train_test_split(x_data, y_data, test_size=val_size,
-                                                                        shuffle=True, random_state=123)
+    def __init__(self, x_data_path="data/mydata/train/noise_train.pt", y_data_path="data/mydata/train/noise_train.pt"):
+        self.x = torch.load(x_data_path)
+        self.y = torch.load(y_data_path)
 
     def __len__(self):
-        if self.mode == "train":
-            return len(self.x_tr)
-        else:
-            return len(self.x_val)
+        return len(self.x)
     
     def __getitem__(self, index):
-        if self.mode == "train":
-            return self.x_tr[index], self.y_tr[index]
-        else:
-            return self.x_val[index], self.y_val[index]
+        return self.x[index], self.y[index]
 
 
 def train(model, dataloader, loss_fn, optimizer, device):
@@ -40,8 +28,7 @@ def train(model, dataloader, loss_fn, optimizer, device):
     for batch, (X, y) in enumerate(dataloader, 1):
         X, y = X.to(device), y.to(device)
         y_pred = model(X)
-        # loss = loss_fn(y_pred, y) + model.R2()
-        loss = _l2(y - y_pred, model.h)
+        loss = loss_fn(y_pred, y) + model.R2()
         ma_loss += (loss.item() * len(y))  # bc. loss_fn predicts avg loss
 
         loss.backward()
@@ -63,8 +50,7 @@ def eval(model, dataloader, loss_fn, device):
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             y_pred = model(X)
-            # loss += (loss_fn(y_pred, y).item() * len(y))
-            loss = _l2(y - y_pred, model.h)
+            loss += (loss_fn(y_pred, y).item() * len(y))
     loss /= data_size
     print(f"Validation/Test error:\n Avg loss: {loss:>8f} \n")
     return loss
@@ -73,9 +59,9 @@ def eval(model, dataloader, loss_fn, device):
 def train_pipeline(model, config):
     with wandb.init(config=config, project="AdaFNN", group=f"{config['model_params']['n_base']}"):
         config = wandb.config
-        train_dataset = CustomDataset(mode="train", x_data_path=config.x_data_path, y_data_path=config.y_data_path)
+        train_dataset = CustomDataset(x_data_path=config.x_train_path, y_data_path=config.y_train_path)
         train_dataloader = DataLoader(train_dataset, config.batch_size, shuffle=True)
-        val_dataset = CustomDataset(mode="val", x_data_path=config.x_data_path, y_data_path=config.y_data_path)
+        val_dataset = CustomDataset(x_data_path=config.x_val_path, y_data_path=config.y_val_path)
         val_dataloader = DataLoader(val_dataset, config.batch_size, shuffle=True)
 
         loss_fn = nn.MSELoss()
